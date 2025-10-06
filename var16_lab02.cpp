c82ba82679240d71fe516e3f3919af0ff2f25d87
skibidi-toilet-1337 <swqdhqh@gmail.com>
2025-10-06 23:58:30 +0700

    Loops with a preface and afterword

--- var16_lab02.cpp
+++ var16_lab02.cpp
@@ -1,23 +1,25 @@
-/**********************
-* Автор: Бобкова Е.А. *
-* Вариант: 16         *
-***********************/
-
+/***********************
+ * РђРІС‚РѕСЂ: Р‘РѕР±РєРѕРІР° Р•.Рђ. *
+ * Р’Р°СЂРёР°РЅС‚: 16         *
+ ***********************/
+ 
 #include <iostream>
 #include <cmath>
 
 using namespace std;
 
 int main() {
-
-    double h0 = 0.05;
-    double V10 = 7.5;
-    double V;  
+	
+  double h0 = 0.05;
+  double V10 = 7.5;
+  double V;  
+  double h = 0;
+  
+  cout << "h: " << "\t" << "V: " << endl;
     
-    cout << "h: " << "\t" << "V: " << endl;
-
-    for (int h: {2, 4, 6, 8, 10, 30, 50, 70, 90}) {
-      V = V10 * (pow(log(h / h0), 2) / pow(log(10 / h0), 2));
-      cout << h << "\t" << V << endl;
-    }
+  do {
+    h+=(h<10)?2:10;  
+    V = V10 * (pow(log(h / h0), 2) / pow(log(10 / h0), 2));
+    cout << h << "\t" << V << endl;
+    } while (h<90);
 }
