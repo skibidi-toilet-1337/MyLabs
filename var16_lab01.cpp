cd9af89cc2855e4de89dd1726c350036c4f56468
skibidi-toilet-1337 <swqdhqh@gmail.com>
2025-10-06 23:50:15 +0700

    Linear algorithm

--- var16_lab01.cpp
+++ var16_lab01.cpp
@@ -8,36 +8,33 @@
 
 using namespace std;
 
-const double Pi = 3.14159265358979323846;
-
-double DegreesToRadians(double degrees) {
-    return degrees * (Pi/180);
+const double pi = 3.14159265358979323846;
+const double g = 9.8;
+  
+double degreesToRadians(double degrees) {
+    return degrees * (pi / 180);
 }
   
 int main() {
-
+	
   double m = 2.145;
   double a = 4.3;
   double b = 6.6;
-  const double g = 9.8;
-  
-  double phi = 20;
+  double phi = 20.0;
       
-  phi = DegreesToRadians(phi);
+  phi = degreesToRadians(phi);
   
   double gamma1, gamma2, T1, T2;
   
   gamma1 = atan(a / g);
   T1 = m * sqrt(a * a + g * g);
  
-  gamma2 = atan( - (( b * cos(phi)) / (g * (( 1 - b * sin(phi) / g) ))));
+  gamma2 = atan(-(( b * cos(phi)) / (g * ((1 - b * sin(phi) / g)))));
   T2 = m * sqrt( g * g + b * b - 2 * g * b * sin(phi));
   
-  cout << "gamma1 = " << gamma1 << endl;
-  cout << "T1 = " << T1 << endl;
-  
-  cout << "gamma2 = " << gamma2 << endl;
-  cout << "T2 = " << T2 << endl;    
-      
+  cout << "gamma1 = " << gamma1 << endl << "T1 = " << T1 << endl;
+  cout << "gamma2 = " << gamma2 << endl << "T2 = " << T2 << endl;
+   
   return 0;
 }
+
