c8bf11644d13b64ff6d4537c68c2dbdfebae053e
skibidi-toilet-1337 <swqdhqh@gmail.com>
2025-10-10 08:58:05 +0700

    New changes

--- 16var_lab03.cpp
+++ 16var_lab03.cpp
@@ -8,25 +8,22 @@
 
 using namespace std;
 
-const double a = 4.0 / 100;
-const double l = 3.0;
-const double lambda = 0.032;
-const double tWalls = 30.0;
-const double tAir = 150.0; 
-const double nu = 2.3 * pow(10,-5);
 const double g = 9.8;
-
-double W[5]={0.5,1,2,5,10};
+double l = 3.0;
+double lambda = 0.032;
+double tWalls = 30.0;
+double tAir = 150.0; 
+double W;
 
 double calcNu(double Re, double Gr) {
     
     double result;
     
-    if (Re<2200) {
-      result = 0.13 * pow(Re,0.33) * pow(Gr,0.1);
+    if (Re < 2200) {
+      result = 0.13 * pow(Re, 0.33) * pow(Gr, 0.1);
     }
     else { 
-      result = 0.018 * pow(Re,0.88);
+      result = 0.018 * pow(Re, 0.88);
     }
     
     return result;
@@ -36,20 +33,30 @@ int main() {
     
   double Re;
   double Gr;  
+  double Q;
+  double Nu;  
+  double a;
+  double nu;
+  double count;
   
-  Gr = ((tAir - tWalls) / 273.0) * ((g * pow(l,3)) / pow(nu,2)); 
+  cout << "Enter number of variables: ";
+  cin >> count;
+ 
+  a = 4.0 / 100;
+  nu = 2.3 * pow(10,-5);
+  Gr = ((tAir - tWalls) / 273.0) * ((g * pow(l, 3)) / pow(nu, 2)); 
   
-  cout << "W" << "\t" << "Re" << "\t" << "Nu" << "\t" << "Q" << endl;
-  
-  for (int i = 0; i <= 4; ++i) {
+  for (int iteration = 0; iteration < count; ++iteration) {
     
-    Re = (W[i] * a) / nu;
-  
-    double Nu = calcNu(Re,Gr);
-  
-    double Q = 4 * lambda * l * Nu * (tAir - tWalls);
-  
-    cout << W[i] << "\t" << Re << "\t" << Nu << "\t" << Q << endl;
+    cout << "Enter a numeric variable: ";
+    cin >> W;
+    
+    Re = (W * a) / nu;
+    Nu = calcNu(Re, Gr);
+    Q = 4 * lambda * l * Nu * (tAir - tWalls);
+    
+    cout << "W" << "\t" << "Re" << "\t" << "Nu" << "\t" << "Q" << endl 
+         << W   << "\t" << Re   << "\t" << Nu   << "\t" << Q   << endl;
  
   }
 
