0ea1895171780a13027a13e59d977bd6fd1a6a3c
skibidi-toilet-1337 <swqdhqh@gmail.com>
2025-10-12 22:18:58 +0700

    New Changes

--- var16_lab04.cpp
+++ var16_lab04.cpp
@@ -4,53 +4,75 @@
  ***********************/
 
 #include <iostream>
-#include <map>
+#include <stdio.h>
 
 using namespace std;
-const int rows = 3;
-const int columns = 3;
+const int rows = 100;
+const int columns = 100;
+
+int compare(const void* a, const void* b) {
+
+  double dA = *(double*)a;
+  double dB = *(double*)b;
+
+  if (dA > dB) {
+    return 1;
+  }
+  else if (dA == dB) {
+    return 0;
+  }
+  return -1;
+}
 
 int main() {
 
   double maxValue;
-  map <double, unsigned> counter;
   double V[rows][columns];
+  double values[rows * columns];
   double currValue;
+  int numOfRows;
+  int numOfColumns;
+  int index = 0;
+  double prevValue;
+ 
+  cout << "Enter the number of rows: " << endl;
+  cin >> numOfRows;
 
-  for (int row = 0; row < rows; ++row) {
-    for (int column = 0; column < columns; ++column) {
-      cout << "Enter V[" << row << "][" << column << "]:" << endl;
+  cout << "Enter the number of columns: " << endl;
+  cin >> numOfColumns;
+
+  for (int rowIndex = 0; rowIndex < numOfRows; ++rowIndex) {
+    for (int columnIndex = 0; columnIndex < numOfColumns; ++columnIndex) {
+      cout << "Enter V[" << rowIndex << "][" << columnIndex << "]:" << endl;
       cin >> currValue;
-
-      V[row][column] = currValue;
-
-      if (counter.count(currValue) > 0) {
-        ++counter[currValue];
-
-      }
-      else {
-        counter[currValue] = 1;
-      }
-
+      V[rowIndex][columnIndex] = currValue;
+      values[index] = currValue;
+      ++index;
+      
     }
+  }
+
+  qsort(values, numOfRows*numOfColumns, sizeof(double), compare);
+  prevValue = values[0];
+  maxValue = values[0];
+  
+  for (int index = 1; index < numOfRows * numOfColumns; ++index ) {
+    if (prevValue == values[index]) {
+      maxValue = max(values[index], maxValue);
+    }
+    prevValue = values[index];
   }
 
   cout << "Matrix: " << endl;
 
-  for (int row = 0; row < rows; ++row) {
-    for (int column = 0; column < columns; ++column) {
-      cout << V[row][column] << "\t";
+  for (int rowIndex = 0; rowIndex < numOfRows; ++rowIndex) {
+    for (int columnIndex = 0; columnIndex < numOfColumns; ++columnIndex) {
+      cout << V[rowIndex][columnIndex] << "\t";
      
     }
     cout << endl;
   }
-  
-  maxValue = -DBL_MIN;
-  for (auto value : counter) {
-    if (value.second > 1) {
-      maxValue = max(value.first, maxValue);
-    }
-  }
-  cout << "Maximum value repeating more than once: " << maxValue;
 
+  cout << "Maximum value repeating more than once: " << maxValue;
+  
 }
